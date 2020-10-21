c60ed5f7a08b9a5f3b2bdbaaf48ce5cd061a9f5a
sunxiangyu <sunxiangyu@cloudwalk.com>
2020-10-21 17:25:58 +0800

    修改

--- class_day02/d2_flask_abc.py
+++ class_day02/d2_flask_abc.py
@@ -7,16 +7,16 @@
 @Contact:  sunxiangyu@cloudwalk.com
 @License: Mulan PSL
 """
-from flask import Flask, request,render_template
+from flask import Flask, request, render_template
 
 # request，保存你请求数据
-# 初始化application
+# 1、初始化application,--name--表示flask初始化app对象的名字，通过名字得到app现在的位置
 app = Flask(__name__,
             template_folder="test_folder"
             )
 
 
-# 添加路由
+# 2.添加路由，view function
 @app.route("/")
 def index():
     args = request.args
@@ -24,5 +24,8 @@ def index():
     return render_template("index.html")
 
 
-# 运行服务器
-app.run(port=5000)
+# 3.运行服务器
+# 调试，测试，开发，自带，不要用于生产环境
+# __name__好处，
+if __name__ == "__main__":
+    app.run()
